6e9253d3126c7623927285be9638246a93e4e25c
Pietro907 <pietrop.puri@gmail.com>
2023-10-03 15:39:46 +0200

    add icon avada in HeaderBottom and fix the images in folders

--- src/components/HeaderBottom.vue
+++ src/components/HeaderBottom.vue
@@ -1,19 +1,36 @@
 
 <script>
-    export default {
-        name: 'HeaderBottom',
-    }
+export default {
+    name: 'HeaderBottom',
+}
 </script>
 
 
 
 <template>
-    <div>
 
+    <div class="container">
+        <div class="row r_60 d_flex jst_btw">
+
+            <div class="col">
+                <div class="card brands_logo">
+                   <img src="../assets/img/classic shop/classic_shop_logo2x.png" alt="">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card brands_logo">
+                   menu nav
+                </div>
+            </div>
+            <div class="col">
+                <div class="card brands_logo">
+                   button:hover & icon
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
