0eb96eae349d8be600599712dd2a6096f38738c5
Pietro907 <pietrop.puri@gmail.com>
2023-10-04 01:28:00 +0200

    add all fix component MainCollection

--- src/components/MainCollection.vue
+++ src/components/MainCollection.vue
@@ -30,14 +30,42 @@ export default {
             </div>
 
             <div class="col">
-                <div class="card">
+                <div class="card overview">
                     <img src="../assets/img/collections/spring/spring_collection_bg.jpg" alt="">
+
+                    <div class="card collect d_flex f_column align_center">
+
+                        <h2>Spring Collection</h2>
+                        <p>BRIGHT AND COLORFUL</p>
+
+                        <div class="btn">
+
+                            <button class="color" type="button">VIEW MORE</button>
+
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
 
             <div class="col">
-                <div class="card">
+                <div class="card overview">
                     <img src="../assets/img/collections/autumn_collection_bg.jpg" alt="">
+
+                    <div class="card collect d_flex f_column align_center">
+
+                        <h2>Autumn Collection</h2>
+                        <p>RICH AND CONFORTABLE</p>
+
+                        <div class="btn">
+
+                            <button class="color" type="button">VIEW MORE</button>
+
+                        </div>
+
+                    </div>
+
                 </div>
             </div>
 
@@ -63,9 +91,11 @@ export default {
 .card>img {
     width: 100%;
 }
-.overview{
+
+.overview {
     position: relative;
 }
+
 .collect {
     height: 430px;
     padding: 0 0 5rem;
@@ -74,35 +104,41 @@ export default {
     position: absolute;
     top: 280px;
     left: 110px;
-    & h2{
+
+    & h2 {
         color: var(--bg-primary);
         font-size: 35px;
         margin: 0 0 0.5rem
     }
+
     & p {
         color: var(--bg-white-smoke);
         font-size: 15px;
         margin: 0 0 1.5rem
     }
 }
-.btn > button {
+
+.btn>button {
     font-size: 9px;
     padding: 0.6rem 1.5rem;
     border-radius: 20px;
     border-color: var(--bg-primary);
     color: var(--bg-primary);
-} 
+}
 
 /* Utilities */
 .d_flex {
     display: flex;
 }
+
 .f_row {
     flex-direction: row;
 }
+
 .f_column {
     flex-direction: column;
 }
+
 .jst_center {
     justify-content: center;
 }
@@ -125,7 +161,7 @@ export default {
 .bg_gray_light {
     background-color: var(--bg-white-smoke);
 }
+
 .color {
     background-color: #f5f5f523;
-}
-</style>
+}</style>
