7f817408427e475a2c668830b56eb30f751ec8d1
Pietro907 <pietrop.puri@gmail.com>
2023-10-04 16:48:14 +0200

    add new component MainTestimonial

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,6 +5,7 @@ import MainFeaturedProduction from './MainFeaturedProduction.vue';
 import MainCollection from './MainCollection.vue';
 import MainBestSeller from './MainBestSeller.vue';
 import MainNewArrivals from './MainNewArrivals.vue';
+import MainTestimonial from './MainTestimonial.vue';
 
     export default {
         name: 'Main',
@@ -19,6 +20,7 @@ import MainNewArrivals from './MainNewArrivals.vue';
             MainCollection,
             MainBestSeller,
             MainNewArrivals,
+            MainTestimonial,
         }
     }
 </script>
@@ -35,6 +37,8 @@ import MainNewArrivals from './MainNewArrivals.vue';
     <MainBestSeller />
 
     <MainNewArrivals />
+
+    <MainTestimonial />
 
 </template>
 
---
+++ src/components/MainTestimonial.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        name: 'MainTestimonial',
+        
+    }
+</script>
+
+
+<template>
+
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
