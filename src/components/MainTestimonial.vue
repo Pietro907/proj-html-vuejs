31a8a887c1e6300870c0add5d3f11656c1877ebf
Pietro907 <pietrop.puri@gmail.com>
2023-10-04 16:53:42 +0200

    add .container>.new_arrivals

--- src/components/MainTestimonial.vue
+++ src/components/MainTestimonial.vue
@@ -8,6 +8,38 @@
 
 <template>
 
+
+<div class="container">
+    <div class="row">
+        <div class="col d_flex jst_center">
+            
+            <div class="card new_arrivals d_flex f_column jst_center">
+                <h1>Brand New Arrivals</h1>
+                <h5>NEW COLLECTION FORM NEW YORK</h5>
+
+                <div class="btn">
+
+                    <button class="color m_05" type="button">VIEW ALL</button>
+                    <button class="color m_05" type="button">LOOKBOOK</button>
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="square">
+            <i class="fa-solid fa-tag"></i>
+        </div>
+
+        <div class="square1">
+            <span class="color_green small">$</span>
+            <span class="color_green number">39</span>
+            <p>On Sale</p>
+        </div>
+
+    </div>
+</div>
+
     <h1>CIAO</h1>
     <h1>CIAO</h1>
     <h1>CIAO</h1>
@@ -24,5 +56,108 @@
 
 
 <style scoped>
+
+.container {
+    background-image: url(../assets/img/home1/testimonials_home_1_bg.jpg);
+    height: 400px;
+    margin: 7rem 0;
+    background-size: cover;
+    background-position: right;
+    background-repeat: no-repeat;
+    position: relative;
+}
+.new_arrivals {
+    height: 430px;
+    padding: 0 0 5rem;
+    text-align: center;
+    margin: 2rem 0 0;
+    & h1{
+        color: var(--bg-primary);
+        font-size: 50px;
+        margin: 0 0 0.5rem
+    }
+    & h5 {
+        color: var(--bg-gray-light);
+        font-size: 15px;
+        margin: 0 0 1.5rem
+    }
+}
+.btn > button {
+    font-size: 9px;
+    padding: 0.6rem 1.5rem;
+    border-radius: 20px;
+    border-color: var(--bg-primary);
+    color: var(--bg-primary);
+} 
+.color {
+    background-color: #f5f5f523;
+}
+.square {
+    position: absolute;
+    top: -20px;
+    right: 15px;
+    border-radius: 7px;
+    background-color: var(--bg-primary);
+    padding: 1rem;
+    box-shadow: 1px 2px 3px 1px;
+}
+.square1 {
+    position: absolute;
+    top: 40px;
+    right: 15px;
+    border-radius: 7px;
+    background-color: var(--bg-primary);
+    padding: 0.5rem;
+    box-shadow: 1px 2px 3px 1px;
+    & .small {
+        font-size: 9px;
+        position: relative;
+        bottom: 8px;
+    }
+    & .number {
+        font-size: 20px;
+    }
+    & p{
+        font-size: 8px;
+    }
+    
+}
+
+
+
+
+/* Utilities */
+.d_flex {
+    display: flex;
+}
+.f_row {
+    flex-direction: row;
+}
+.f_column {
+    flex-direction: column;
+}
+.jst_center {
+    justify-content: center;
+}
+
+.jst_end {
+    justify-content: end;
+}
+
+.jst_btw {
+    justify-content: space-between;
+}
+
+.align_center {
+    align-items: center;
+}
+.m_05 {
+    margin: 0 0.5rem;
+}
+
+
+/* Color */
+
+
 
 </style>
