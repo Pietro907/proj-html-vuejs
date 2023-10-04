2ec8004e311798b92c140ad30d9e56fe15b14db4
Pietro907 <pietrop.puri@gmail.com>
2023-10-04 21:35:18 +0200

    add draft 4 row in top rates

--- src/components/MainTopRates.vue
+++ src/components/MainTopRates.vue
@@ -1,28 +1,378 @@
 
 
 <script>
-    export default {
-        name: 'MainTopRates',
-        
-    }
+export default {
+    name: 'MainTopRates',
+
+}
 </script>
 
 <template>
-    
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
-<h1>CIAO</h1>
+    <!-- 4 card img -->
+    <div class="container">
+        <div class="row r_60 d_flex jst_center align_center">
 
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+     <!-- 4 card img -->
+     <div class="container">
+        <div class="row r_60 d_flex jst_center align_center">
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+     <!-- 4 card img -->
+     <div class="container">
+        <div class="row r_60 d_flex jst_center align_center">
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+     <!-- 4 card img -->
+     <div class="container">
+        <div class="row r_60 d_flex jst_center align_center">
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card products d_flex">
+
+                    <img src="../assets/img/black_elegant_man/black_elegant_leather_jacket.jpg" alt="">
+
+                    <div class="info_cost">
+
+                        <h5>Black Leather Jacket</h5>
+                        <i class="fa-solid fa-star"></i> <br>
+                        <p class="style_line"><b>$235</b></p>
+                        <p><b>$200</b></p>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
+    <h1>CIAO</h1>
 </template>
 
 <style scoped>
+.r_60 {
+    width: 60%;
+    margin: 0 auto;
+    padding: 2rem 0;
+}
 
+.products>img {
+    width: 70px;
+}
+
+.info_cost>h5 {
+    padding: 1rem 0 0.5rem;
+    color: var(--bg-gray-light);
+}
+
+.info_cost>.fa-star {
+    color: var(--bg-heaven);
+}
+
+p {
+    display: inline-block;
+    padding: 0.5rem 0.5rem 0 0;
+    font-size: 12px;
+}
+
+.style_line {
+    text-decoration-line: line-through;
+    font-size: 9px;
+}
+
+
+/* Utilities */
+.d_flex {
+    display: flex;
+}
+
+.jst_center {
+    justify-content: center;
+}
+
+.jst_end {
+    justify-content: end;
+}
+
+.jst_btw {
+    justify-content: space-between;
+}
+
+.align_center {
+    align-items: center;
+}
+
+
+/* Color */
 </style>
